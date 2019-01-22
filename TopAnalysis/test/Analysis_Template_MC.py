7fa15c1e5dd5f64fef6be1d3febb50e5523733c4
jabuschh <44866552+jabuschh@users.noreply.github.com>
2019-01-22 12:52:14 +0100

    Update Analysis_Template_MC.py

--- TopAnalysis/test/Analysis_Template_MC.py
+++ TopAnalysis/test/Analysis_Template_MC.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-                                                                                                                                                                                      
+# -*- coding: utf-8 -*-
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("myprocess")
 process.TFileService=cms.Service("TFileService",fileName=cms.string("Test.root"))
 
-##-------------------- Define the source  ----------------------------                                                                                                                                       
+##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
         )
@@ -12,18 +12,15 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("EmptySource")
 
 
-##-------------------- User analyzer  --------------------------------                                                                                                                                       
+##-------------------- User analyzer  --------------------------------
 process.analysis  = cms.EDAnalyzer('Analysis_Template_MC',
-                                   filename        = cms.string('file:GenLevelOutput.root'),
+                                   filename        = cms.string('file:GenLevelOutput_test.root'),
                                    treename        = cms.string('events'),
                                    dirname         = cms.string('boosted'),
                                    isMCarlo        = cms.untracked.bool(True),
-                                   CrossSection    = cms.untracked.double(100),
+                                   CrossSection    = cms.untracked.double(0.017),
                                    IntLumi         = cms.untracked.double(100),
                                    Triggers        = cms.untracked.vstring(''),
                                    )
 
 process.p = cms.Path(process.analysis)
-
-
-
