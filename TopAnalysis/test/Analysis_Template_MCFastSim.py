797c02449ae48bb9c37b2426562fe36478f33ce2
jabuschh <44866552+jabuschh@users.noreply.github.com>
2019-02-01 10:15:50 +0100

    Update Analysis_Template_MCFastSim.py

--- TopAnalysis/test/Analysis_Template_MCFastSim.py
+++ TopAnalysis/test/Analysis_Template_MCFastSim.py
@@ -6,21 +6,23 @@ process.TFileService=cms.Service("TFileService",fileName=cms.string("Test.root")
 
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(1)
-        )
+input = cms.untracked.int32(1)
+)
 
 process.source = cms.Source("EmptySource")
 
-
 ##-------------------- User analyzer  --------------------------------
-process.analysis  = cms.EDAnalyzer('Analysis_Template_MCFastSim',
-                                   filename        = cms.string('file:/nfs/dust/cms/user/gunnep/DelphesAnalysis/ntuplizer/output/LG_1300_S130_ntuple.root'),
-                                   treename        = cms.string('Delphes'),
-                                   dirname         = cms.string(''),
-                                   isMCarlo        = cms.untracked.bool(True),
-                                   CrossSection    = cms.untracked.double(0.00009817),
-                                   IntLumi         = cms.untracked.double(100),
-                                   Triggers        = cms.untracked.vstring(''),
-                                   )
+process.analysis = cms.EDAnalyzer('Analysis_Template_MCFastSim',
+filename         = cms.string('file:/nfs/dust/cms/user/gunnep/DelphesAnalysis/ntuplizer/output/LG_1300_S130_ntuple.root'),
+treename         = cms.string('Delphes'),
+dirname          = cms.string(''),
+isMCarlo         = cms.untracked.bool(True),
+CrossSection     = cms.untracked.double(0.00009817),
+IntLumi          = cms.untracked.double(100),
+Triggers         = cms.untracked.vstring(''),
+ElSelection      = cms.untracked.bool(False),
+MuSelection      = cms.untracked.bool(False),
+HadSelection     = cms.untracked.bool(True),
+)
 
 process.p = cms.Path(process.analysis)
